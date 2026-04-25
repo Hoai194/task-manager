17c2e4450c4f49e85d4b43a12b60dd5a35fd8555
Trần Thị Hoài <tranhoai194205@gmail.com>
2026-04-25 23:34:21 +0700

    Update userRoute.js

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -6,11 +6,15 @@ import {
   updateProfile, 
   removeAccount 
 } from '../controllers/userController.js'
+import auth from '../middleware/auth.js'
 
 const userRouter = Router();
 
 userRouter.post('/register', register);
 userRouter.post('/login', login);
+
+userRouter.use(auth);
+
 userRouter.get('/me', getProfile);
 userRouter.put('/me', updateProfile);
 userRouter.delete('/me', removeAccount); 
